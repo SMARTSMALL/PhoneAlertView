3a8e4fc61c1d47a5240555e9dc4295604c154f42
smart_small <smart_small@163.com>
2017-03-14 10:25:28 +0800

    Initial Commit

---
+++ PhoneAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhoneAlertView
+//
+//  Created by goldeneye on 2017/3/14.
+//  Copyright © 2017年 goldeneye by smart-small. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PhoneAlertView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PhoneAlertView
+//
+//  Created by goldeneye on 2017/3/14.
+//  Copyright © 2017年 goldeneye by smart-small. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
